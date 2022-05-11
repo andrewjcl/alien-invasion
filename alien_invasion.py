ea4c64d29577aa6c77876d1b18ae32c835a80ea1
Andrew Lim <andrewjcl88@gmail.com>
2022-05-12 09:45:45 +1000

    Update alien_invasion.py

--- alien_invasion.py
+++ alien_invasion.py
@@ -8,9 +8,13 @@ class AlienInvasion:
     def __init__(self):
         """ Initialise the game, and create game resources. """
         pygame.init()
+        self.clock = pygame.time.Clock()
 
         self.screen = pygame.display.set_mode((1200, 800))
         pygame.display.set_caption("Alien Invasion")
+
+        # Set the background color
+        self.bg_color = (230, 230, 230)
 
     def run_game(self):
         """ Start the main game loop """
@@ -20,10 +24,14 @@ class AlienInvasion:
                 if event.type == pygame.QUIT:
                     sys.exit()
 
+            # Redraw the screen during each pass through the loop
+            self.screen.fill(self.bg_color)
+
             # Make the most recently drawn screen visible
             pygame.display.flip()
+            self.clock.tick(1)
 
 if __name__ == '__main__':
     # Make a game instance, and run the game
     ai = AlienInvasion()
-    ai.run_game
+    ai.run_game()
