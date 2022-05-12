8f0d49803ea41499796e61cccda099b4138af4a6
Andrew Lim <andrewjcl88@gmail.com>
2022-05-13 07:36:49 +1000

    Added ship, movement

--- alien_invasion.py
+++ alien_invasion.py
@@ -1,6 +1,8 @@
 import sys
 
 import pygame
+from settings import Settings
+from ship import Ship
 
 class AlienInvasion:
     """ Overall class to manage game assets and behaviour. """
@@ -8,28 +10,45 @@ class AlienInvasion:
     def __init__(self):
         """ Initialise the game, and create game resources. """
         pygame.init()
+        self.settings = Settings()
         self.clock = pygame.time.Clock()
-
-        self.screen = pygame.display.set_mode((1200, 800))
+        
+        self.screen = pygame.display.set_mode(
+            (self.settings.screen_width, self.settings.screen_height))
         pygame.display.set_caption("Alien Invasion")
 
-        # Set the background color
-        self.bg_color = (230, 230, 230)
+        self.ship = Ship(self)
 
     def run_game(self):
         """ Start the main game loop """
         while True:
             # Listen for keyboard and mouse events
             for event in pygame.event.get():
+                
                 if event.type == pygame.QUIT:
                     sys.exit()
+                
+                elif event.type == pygame.KEYDOWN:
+                    if event.key == pygame.K_RIGHT:
+                        self.ship.moving_right = True
+                    elif event.key == pygame.K_LEFT:
+                        self.ship.moving_left = True
+                
+                elif event.type == pygame.KEYUP: 
+                    if event.key == pygame.K_RIGHT:
+                        self.ship.moving_right = False
+                    elif event.key == pygame.K_LEFT:
+                        self.ship.moving_left = False
+                
+            self.ship.update()
 
             # Redraw the screen during each pass through the loop
-            self.screen.fill(self.bg_color)
+            self.screen.fill(self.settings.bg_color)
+            self.ship.blitme()
 
             # Make the most recently drawn screen visible
             pygame.display.flip()
-            self.clock.tick(1)
+            self.clock.tick(60)
 
 if __name__ == '__main__':
     # Make a game instance, and run the game
---
+++ settings.py
@@ -0,0 +1,8 @@
+class Settings:
+    def __init__(self):
+        """ A class to store all settings """
+        # Screen settings
+        self.screen_width = 1200
+        self.screen_height = 800
+        self.bg_color = (230, 230, 230)
+        self.ship_speed = 1.5
